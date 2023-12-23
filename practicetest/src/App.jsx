6b8604d59a6b361d43b601f4d87773a91534b9bf
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-12-24 00:26:07 +0530

    addd

--- practicetest/src/App.jsx
+++ practicetest/src/App.jsx
@@ -3,6 +3,9 @@ import LoginRegister from './Components/LoginRegister';
 import ProductsPage from './Components/ProductsPage';
 import SingleProduct from './Components/SingleProduct';
 import Todo from './Components/Todo';
+import Register from './Components/Register';
+import ProductListing from './Components/ProductListing';
+import IndividualProduct from './Components/IndividualProduct';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Route exact path='/products' element={<ProductsPage />} />
         <Route exact path='/products/:id' element={<SingleProduct />} />
         <Route exact path='/todooo' element={<Todo />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/productlisting' element={<ProductListing />} />
+        <Route exact path='/productlisting/:id' element={<IndividualProduct />} />
       </Routes>
     </div>
   );
